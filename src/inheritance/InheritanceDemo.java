d696c17a56946f18e9a81a983cdd0ac70fd5e1a4
dbc2201 <divyanshb22@gmail.com>
2020-08-24 11:30:53 +0530

    add definition of Inheritance in Object-Oriented programming

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -14,11 +14,19 @@ public class InheritanceDemo {
     // Inheritance: noun -> something that is or may be inherited.
     // Inheritance: verb -> the act of receiving or deriving an inheritance (noun).
 
+    // Inheritance (verb) is the act of acquiring some properties, attributes, traits,
+    // behaviour ... etc by one entity from another entity related to it.
+    // entity -> human being, living thing, non-living thing
+
     // Example of this type of inheritance from the real-world:
     // 1. Some money, A house, a car or some piece of land ... etc, that is inherited
     // by the CHILDREN (YOU) from THE PARENTS (YOUR PARENTS), if they so choose.
     // 2. The physical attributes, color of skin, height, hair color, color of eyes ... etc
     // is inherited by YOU from YOUR PARENTS.
+
+    // Inheritance (Object-Oriented): Entity is a class!
+    // Inheritance (noun) is the mechanism by which a class can acquire
+    // properties (fields/variables) and/or methods of another class related to it.
 
     // 2. We can use Inheritance to re-use code/programs/software that is already developed and tested.
     // 3. Observe the use of the `extends` keyword.
