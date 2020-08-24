b4b6eff6aa8dd4f102f8e2d3c2de36524c2a9b98
dbc2201 <divyanshb22@gmail.com>
2020-08-24 11:21:24 +0530

    add examples of inheritance in real life

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -8,6 +8,18 @@ package inheritance;
 
 public class InheritanceDemo {
     // 1. What is Inheritance?
+
+    // Inheritance (Simple English) :
+    // Both a noun and a verb.
+    // Inheritance: noun -> something that is or may be inherited.
+    // Inheritance: verb -> the act of receiving or deriving an inheritance (noun).
+
+    // Example of this type of inheritance from the real-world:
+    // 1. Some money, A house, a car or some piece of land ... etc, that is inherited
+    // by the CHILDREN (YOU) from THE PARENTS (YOUR PARENTS), if they so choose.
+    // 2. The physical attributes, color of skin, height, hair color, color of eyes ... etc
+    // is inherited by YOU from YOUR PARENTS.
+
     // 2. We can use Inheritance to re-use code/programs/software that is already developed and tested.
     // 3. Observe the use of the `extends` keyword.
     // 4. What is `super` class, and what is a `sub` class.
