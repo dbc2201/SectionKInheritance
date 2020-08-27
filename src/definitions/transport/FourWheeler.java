6170d1abb80e47a73ed55f85e86af19bfc086da8
dbc2201 <divyanshb22@gmail.com>
2020-08-27 10:13:57 +0530

    create FourWheeler.java

---
+++ src/definitions/transport/FourWheeler.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 27/08/20
+ *  Time: 10:13 AM
+ *  File Name : FourWheeler.java
+ * */
+package definitions.transport;
+
+public class FourWheeler {
+}
--- src/definitions/transport/Vehicle.java
+++ src/definitions/transport/Vehicle.java
@@ -6,5 +6,13 @@
  * */
 package definitions.transport;
 
-public class Vehicle {
+public class Vehicle { // parent class ? or a child class ?
 }
+
+// Since, there is no other class to relate this class to,
+// we cannot say right now that whether this class is a parent class
+// or a child class.
+
+// This class will only become a parent class when it has a child class!
+
+// Right now, this is just a simple definition class. okay?
