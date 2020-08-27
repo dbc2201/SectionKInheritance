f800885ad084a811a608105bb8e0330f115d0d44
dbc2201 <divyanshb22@gmail.com>
2020-08-27 10:19:56 +0530

    add example for simple inheritance

--- src/definitions/transport/FourWheeler.java
+++ src/definitions/transport/FourWheeler.java
@@ -14,3 +14,8 @@ public class FourWheeler extends Vehicle {
 // Vehicle class is the parent class of FourWheeler class.
 // Now, the Vehicle class has become the parent class, and
 // the FourWheeler class has become the child class. ok?
+
+// In this example, we can see, that there is a single parent class,
+// called the Vehicle class and there is a single child class
+// called the FourWheeler class.
+// This is an example of Single (Simple) Inheritance.
