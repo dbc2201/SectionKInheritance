71c0fa6740718579b78b716a2051481d1b4e574d
dbc2201 <divyanshb22@gmail.com>
2020-08-29 15:07:50 +0530

    add a method to display the values of a SuperBike object

--- src/definitions/examples/Example5.java
+++ src/definitions/examples/Example5.java
@@ -24,4 +24,11 @@ class Bike {
 
 class SuperBike extends Bike {
     int maxSpeed;
+
+    @Override
+    void display() {
+        System.out.println("modelNumber = " + modelNumber);
+        System.out.println("modelYear = " + modelYear);
+        System.out.println("maxSpeed = " + maxSpeed);
+    }
 }
