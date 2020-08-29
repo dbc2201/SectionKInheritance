8f638d7204b025b758e324d68865e6cebe755928
dbc2201 <divyanshb22@gmail.com>
2020-08-29 10:05:09 +0530

    create Example2.java

---
+++ src/definitions/examples/Example2.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 29/08/20
+ *  Time: 10:04 AM
+ *  File Name : Example2.java
+ * */
+package definitions.examples;
+
+public class Example2 {
+}
