52770603b0e98b962ff4ef4871b644674a137f59
dbc2201 <divyanshb22@gmail.com>
2020-08-31 11:08:34 +0530

    create class Binding

---
+++ src/definitions/bindings/Binding.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 31/08/20
+ *  Time: 11:08 AM
+ *  File Name : Binding.java
+ * */
+package definitions.bindings;
+
+public class Binding {
+}
