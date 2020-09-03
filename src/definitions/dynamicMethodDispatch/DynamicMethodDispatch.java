891357103c9ccfef0e7c3163f23ad361c54870af
dbc2201 <divyanshb22@gmail.com>
2020-09-03 10:06:41 +0530

    create DynamicMethodDispatch.java

---
+++ src/definitions/dynamicMethodDispatch/DynamicMethodDispatch.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 03/09/20
+ *  Time: 10:06 AM
+ *  File Name : DynamicMethodDispatch.java
+ * */
+package definitions.dynamicMethodDispatch;
+
+public class DynamicMethodDispatch {
+}
