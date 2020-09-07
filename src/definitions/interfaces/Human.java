8aabc802ce72187b94016eddf0b0fd0be1937bcb
dbc2201 <divyanshb22@gmail.com>
2020-09-07 11:13:58 +0530

    add rules for creating fields in an interface

--- src/definitions/interfaces/Human.java
+++ src/definitions/interfaces/Human.java
@@ -7,4 +7,18 @@
 package definitions.interfaces;
 
 public interface Human {
+
+    // for fields of the interface
+    // i. every field declared in an interface must be initialized.
+    // ii. every field declared in an interface is automatically considered
+    // as public by the compiler.
+    // iii. every field declared in an interface is automatically considered
+    // as final by the compiler i.e we cannot change its value.
+    // iv. every field declared in an interface is automatically considered
+    // as static by the compiler.
+    // public final static <dataType> <variableName> = <initialValue>;
+    int number = 4;
+
+    // for methods of the interface
+
 }
